d0f9f59d9fb868297c8495a6ba589ff26bba0f23
Slydog9696 <DCEntrpris@hotmail.com>
2023-08-21 22:38:24 -0500

    Revised Files

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
+
 function increment() {
     count += 1
     countEl.textContent = count
 }
+increment()
+console.log(count)
 
 function save() {
     let countStr = count + " - "
@@ -13,3 +16,7 @@ function save() {
     countEl.textContent = 0
     count = 0
 }
+save()
+console.log(count)
+let countStr = 0
+saveEl.textContent = "Previous entries: "
